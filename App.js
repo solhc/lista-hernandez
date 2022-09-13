2f7cde99275b127668857400c7f7e5129580165f
marisol <hcmarisol@uaem.mx>
2022-09-13 11:06:46 -0500

    lista-optimizada

--- App.js
+++ App.js
@@ -7,28 +7,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
   },
-
-  itemContainer: {
-    flex: 1,
-    marginVertical: 5,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-    backgroundColor: '#9F84BD',
-    paddingHorizontal: 10,
-    paddingVertical: 20,
-    borderRadius: 5,
-  },
-  item: {
-    fontSize: 16,
-    color: '#000000',
-  },
-  delete: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#ffffff'
-  },
   modalContainer: {
     justifyContent: 'center',
     alignItems: 'center',
